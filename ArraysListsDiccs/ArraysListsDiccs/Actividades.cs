36dc0619a4222c0432e286bf1bf406fcc06210c1
Daiana Natali Sepúlveda <62265444+DaianaSep@users.noreply.github.com>
2021-04-08 18:00:10 -0300

    Hasta actividad 40

---
+++ ArraysListsDiccs/ArraysListsDiccs/Actividades.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysListsDiccs
+{
+    class Actividades
+    {
+        public void A39()
+        {
+            bool IngresoCorrecto = false;
+            int[] ArrayIngreso = new int[5];
+
+            do
+            {
+                for (int i = 0; i < ArrayIngreso.Length; i++)
+                {
+                    IngresoCorrecto = false;
+                    while (!IngresoCorrecto)
+                    {
+                        Console.Write("Ingrese un número en la posición {0} ", i + 1);
+                        string Ingreso = Console.ReadLine();
+                        bool ok = !int.TryParse(Ingreso, out ArrayIngreso[i]);
+                        if (ok)
+                        {
+                            Console.WriteLine("Debe ingresar valores numéricos");
+                        }
+                        else
+                        {
+                            IngresoCorrecto = true;
+                        }
+                    }
+                }
+
+            } while (!IngresoCorrecto);
+
+            Array.Reverse(ArrayIngreso, 0, 5);
+
+            foreach (int i in ArrayIngreso)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public void A40()
+        {
+            bool Ok = false;
+            string Ingreso;
+           
+            do
+                {
+                Console.WriteLine("Ingrese una frase: ");
+                Ingreso = Console.ReadLine();
+
+                Ok = !string.IsNullOrEmpty(Ingreso);
+                if(!Ok)
+                {
+                    Console.WriteLine("Debe ingresar una frase");
+                }
+                
+            } while (!Ok);
+
+            string[] Palabras = Ingreso.Split(' ');
+            foreach(string i in Palabras)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ArraysListsDiccs/ArraysListsDiccs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysListsDiccs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actividades A = new Actividades();
+            //A.A39();
+            A.A40();
+            Console.ReadKey();
+
+        }
+    }
+}
